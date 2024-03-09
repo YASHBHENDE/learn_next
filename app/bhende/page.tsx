d462639642178210359008bdb0e0053c1e63fb88
YASHBHENDE <yash.abhay.bhende@gmail.com>
2024-03-09 16:18:39 +0530

    changes

---
+++ app/bhende/page.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Page(){
+    return <>
+        <h1>this is for '/bhende'</h1>
+    </>
+}
---
+++ app/bhende/yash/page.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
+import Search from "@/app/ui/search"
+export default function Page(){
+    return <>
+        <h1>this is for route 'bhende/yash'</h1><br />
+
+        <Search placeholder={"search mother"}/>
+    </>
+}
